dbcc35308f6c908ecd279d886cba9ea9f3ed8f89
Amelia Crate <acrate@waldn.net>
2025-03-08 00:21:59 -0800

    limit bootloader to 10 generations

--- configuration.nix
+++ configuration.nix
@@ -29,6 +29,7 @@ in
   boot.initrd.availableKernelModules = [ "xhci_pci" "xhci_hcd" "nvme" "usbhid" "usb_storage" "uas" "sd_mod" ];
 
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "ntfs" ];
 
